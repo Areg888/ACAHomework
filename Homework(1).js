4e4a93779552cd5567ab0df993542a298a9ea324
BossAreg <79797125+BossAreg@users.noreply.github.com>
2021-05-18 17:34:58 +0400

    Add files via upload

---
+++ Homework(1).js
@@ -0,0 +1,45 @@
+// Ex. 1
+
+let yourName = prompt("What is your name?");
+let surname = prompt("What is Your surname?");
+alert("Your name is " + yourName + " and your surname is " + surname);
+
+// Ex. 2
+
+let a = 15;
+let b = 10;
+
+alert(a+b);
+alert(a-b);
+alert(a*b);
+alert(a/b);
+alert(a%b);
+ 
+// Ex. 3
+
+let d = 154568;
+console.log(d % 10);
+
+
+// Ex. 4
+
+let myName = "Areg";
+let age = 17;
+
+console.log("My name is " + myName + ", I am " + age); 
+
+// Ex. 5
+
+let x = 5;
+let y = 10;
+
+x = x + y; // x = 5 + 10  // x = 15
+y = x - y; // y = 15 - 10 // y = 5
+x = x - y; // x = 15 - 5  // x = 10
+
+alert(x); // 10
+alert(y); // 5
+
+
+
+ 
