bdfeeb704bbbc4a34610be17a6eec30d02a37923
Areg Budaghyan <79797125+BossAreg@users.noreply.github.com>
2021-06-17 11:24:44 +0400

    Update Homework(5).js

--- Homework(5).js
+++ Homework(5).js
@@ -1,6 +1,6 @@
 // Ex.1
 
-function sorting(z,x,y) {
+function sorting(z, x, y) {
     for (let i = 0; i < z.length; i++) {
         if (z[i] % 2 == 0) {
             x.push(z[i]);
@@ -12,7 +12,7 @@ function sorting(z,x,y) {
     alert(y);
 }
 
-sorting([1,2,3,4,5,6], [], []);
+sorting([1, 2, 3, 4, 5, 6], [], []);
 
 // Ex.2
 
@@ -29,7 +29,7 @@ function anagram(y, x) {
     }
 }
 
-anagram("invest",["bank", "table", "isevtn", "note", "pencil"]);
+anagram("invest", ["bank", "table", "isevtn", "note", "pencil"]);
 
 // Ex. 3
 
@@ -37,27 +37,35 @@ function strings(str1, str2) {
     alert(str1.replaceAll(str2, ""));
 }
 
-strings(prompt("Write some text"),prompt("Write key word"));
+strings(prompt("Write some text"), prompt("Write key word"));
 
 //Ex. 4
 
-let firstArray = [4,4];
+let firstArray = [4, 4];
 let lastArray = [];
 let count = 0;
 let check = false;
 
 for (i = 0; i < firstArray.length; i++) {
-	for (j = 0; j < lastArray.length; j++) {
-		if ( firstArray[i] == lastArray[j] ) {
-			check = true;
-		}
-	}
-	count++;
-	if (count == 1 && check == false) {
-		lastArray.push(firstArray[i]);
-	}
-	check = false;
-	count = 0;
+    for (j = 0; j < lastArray.length; j++) {
+        if (firstArray[i] == lastArray[j]) {
+            check = true;
+        }
+    }
+    count++;
+    if (count == 1 && check == false) {
+        lastArray.push(firstArray[i]);
+    }
+    check = false;
+    count = 0;
 }
 
 alert(lastArray);
+
+//Ex. 5
+
+function strings(str1, str2, str3) {
+    alert(str1.replaceAll(str2, str3));
+}
+
+strings(prompt("Write some text"), prompt("Write any leter"), prompt("Write any symbol"));
